7319af6a15e4f747b1a547aff0f9464b165d2a8f
henuliulei <1957598452@qq.com>
2022-01-11 20:46:25 +0800

    dev

--- src/main/java/com/example/controller/hello2Controller.java
+++ src/main/java/com/example/controller/hello2Controller.java
@@ -1,4 +1,14 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hello2Controller {
+
+    @RequestMapping("/test")
+    public void test(){
+        System.out.println("hello");
+    }
+
 }
